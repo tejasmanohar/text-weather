a4455a15c488b65675d87652cfee681ef54441c5
Tejas Manohar <me@tejasmanohar.com>
2014-12-14 17:57:14 -0600

    server

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
+// require express
 var express = require('express');
+// initialize app
 var app = express();
-var port = process.env.PORT || 3000;
-var client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 
+// server port
+var port = process.env.PORT || 3000;
+// twilio client
+var client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
+// superagent module
+var request = require('superagent');
+
+// express router
 var router = express.Router();
 
+// accept POST on /receive
 router.post('/receive', function(req, res) {
+  // log sms body
+  console.log(req.param('Body'));
 });
 
+// server listens
 app.listen(port);
+
+// signals server has started
 console.log('Running on port ' + port);
