228ee5eb8f70b570510ee5c03270d8305b4c2056
Tejas Manohar <me@tejasmanohar.com>
2014-12-14 17:52:50 -0600

    server setup

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
 var client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
+
+var router = express.Router();
+
+router.post('/receive', function(req, res) {
+});
+
+app.listen(port);
+console.log('Running on port ' + port);
