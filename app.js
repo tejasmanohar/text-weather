3524406a7878b37debd98c7c87cf5e3430046f81
Tejas Manohar <me@tejasmanohar.com>
2014-12-14 18:08:28 -0600

    request

--- app.js
+++ app.js
@@ -15,7 +15,24 @@ var router = express.Router();
 
 // accept POST on /receive
 router.post('/receive', function(req, res) {
-  // log sms body
+  request
+  .get('http://api.openweathermap.org/data/2.5/weather?q=' + req.param('Body') + '&APPID=' + process.env.APP_ID)
+  .set('Accept', 'application/json')
+  .end(function(error, res){
+    client.sendMessage({
+
+      to:req.param('From'),
+      from: process.env.TWILIO_NUMBER,
+      body: res.text.main.temp
+
+    }, function(err, responseData) {
+
+      if (!err) {
+        console.log(responseData.from);
+        console.log(responseData.body);
+      }
+    });
+  });
   console.log(req.param('Body'));
 });
 
